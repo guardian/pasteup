a88581b7973966f40652c193a9a38ce0ef6b6171
Andy Hume <andyhume@gmail.com>
2012-03-20 10:09:16 +0000

    Added build for individual module files.

--- build/build_docs.py
+++ build/build_docs.py
@@ -1,36 +1,66 @@
 #!/usr/bin/env python
 
+# build_docs.py - Builds individual Pasteup module files into one HTML page.
+
 import os
+import sys
 
-from jinja2 import Environment, FileSystemLoader
+try:
+	from jinja2 import Environment, FileSystemLoader
+except ImportError:
+	print "Cannot find jinja2 templating. Install with pip install jinja2."
+	sys.exit()
 
-env = Environment(loader=FileSystemLoader('templates'))
-base_template = env.get_template('base.html')
+# Assumes we're running in /build. We'll bail out somewhere if not.
+TEMPLATE_DIR = 'templates'
+MODULE_DIR = '../content/module/'
+MODULE_LIB = '../docs/modules.html'
+MODULE_PAGES_DIR = '../docs/modules/'
 
-modules = []
+def build_module_library():
 
-for file in os.listdir('../content/module'):
+	try:
+		env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+		base_template = env.get_template('library.html')
+	except:
+		print "Cannot find template. Are you running script from /build?"
 
-	f = open('../content/module/' + file, 'r')
-	module = {}
-	module['name'] = file
-	module['code'] = ''.join(f.readlines())
-	f.close()
+	modules = []
+	for file in os.listdir(MODULE_DIR):
+		f = open(MODULE_DIR + file, 'r')
+		modules.append({
+			'name': file,
+			'code': ''.join(f.readlines())
+		})
+		f.close()
 
-	modules.append(module)
+	output = base_template.render(modules=modules)
 
-output = base_template.render(modules=modules)
+	mod_file = open(MODULE_LIB, 'w')
+	mod_file.write(output)
+	mod_file.close()
 
-mod_file = open('../docs/modules.html', 'w')
-mod_file.write(output)
-mod_file.close()
+def build_module_pages():
+
+	try:
+		env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+		module_template = env.get_template('module.html')
+	except:
+		print "Cannot find template. Are you running script from /build?"
+
+	for file in os.listdir(MODULE_DIR):
+		f = open(MODULE_DIR + file, 'r')
+		module = {
+			'name': file,
+			'code': ''.join(f.readlines())
+		}
+		output = module_template.render(module=module)
+		mod_file = open(MODULE_PAGES_DIR + file, 'w')
+		mod_file.write(output)
+		mod_file.close()
 
 
-
-
-
-
-
-
-
-
+if __name__ == '__main__': 
+	# TODO: Add command line option to build individual files as well.
+	build_module_library()
+	#build_module_pages()
