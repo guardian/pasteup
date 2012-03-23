b2b47df374cf540d5fce1308e8ce13d5fc776b9f
James Gorrie <james.gorrie@guardian.co.uk>
2012-03-23 17:17:19 +0000

    Changed setup to use hardcoded node file

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
-
-echo "Installing nodejs and npm"
+echo "Installing GCC"
 echo "==============================================="
-sudo aptitude install nodejs
-curl http://npmjs.org/install.sh > install_npm.sh
-chmod 775 install_npm.sh
-sudo ./install_npm.sh
-rm install_npm.sh
+sudo apt-get install gcc
+
+echo "Installing nodejs"
+echo "==============================================="
+mkdir ~/node-install
+cd ~/node-install
+curl -O http://nodejs.org/dist/node-v0.6.13.tar.gz
+
+tar xvf node-v0.6.13.tar.gz
+cd node-v0.6.13
+
+./configure
+sudo make install
+
+echo "Installing npm"
+echo "==============================================="
+curl http://npmjs.org/install.sh | sudo sh
